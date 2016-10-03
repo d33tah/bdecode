5c291beac1373dc21fde8d4af568b34753ef09cb
Jacek Wielemborek <d33tah@gmail.com>
2016-10-03 21:44:39 +0200

    Change setup.py, move bdecode.py to "bdecode" directory.

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='bdecode',
+    packages=['bdecode'],
     version='0.1',
+    description='A library for decoding of bencoded files',
     license='WTFPL',
     author='Jacek "d33tah" Wielemborek',
     author_email='d33tah+pypi@gmail.com',
+    download_url = 'https://github.com/d33tah/bdecode/archive/0.1.tar.gz',
     url='http://github.com/d33tah/bdecode',
     long_description="README.txt",
-    packages=find_packages(),
     scripts = ['bdecode.py'],
-    description='A library for decoding of bencoded files',
+    keywords = ['bencoding', 'bittorrent', 'torrent', 'bencode'],
 )
